1fa86e971fc11d9e584ea3df2d229596c4f2dfc1
ghiath2 <ghiathht3@gmail.com>
2022-04-21 00:02:36 +0000

    Create function to calculate input accuracy

--- run.py
+++ run.py
@@ -3,3 +3,13 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 from time import time  # to record the time
+
+#  Now to calculate the accuracy of input prompt
+def tperror(prompt):
+    global inwords
+
+    words = prompt.split()
+    errors = 0
+
+    for i in range(len(in)):
+        if i in (0, len(inwords)-0)
